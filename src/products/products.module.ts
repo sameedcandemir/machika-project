8209d9584e3e5cbd71371d489d70583bdfdf841d
Samed <websamedcandemir@gmail.com>
2026-03-14 14:23:43 +0300

    ProductsModule icine CloudinaryModule eklendi

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module'; // 👈 1. Bulut modülünü içeri alıyoruz
 
 @Module({
+  imports: [
+    CloudinaryModule, // 👈 2. Buraya ekleyerek Controller'ın bu servisi kullanmasına izin veriyoruz
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService, PrismaService],
+  providers: [ProductsService], // Not: PrismaService genelde PrismaModule içinden gelir, burada tekrar yazmaya gerek kalmaz
 })
 export class ProductsModule {}
