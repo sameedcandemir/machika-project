f94013bb69794b8bb6a0737b7acbed8d46849efe
Samed <websamedcandemir@gmail.com>
2026-04-02 15:24:33 +0300

    AppController module icine kaydedildi, 404 cozumu

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller'; // 👈 YENİ: Kapımızı merkeze çağırıyoruz
+import { AppService } from './app.service';       // 👈 YENİ: İşçimizi merkeze çağırıyoruz
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
-import { UsersModule } from './users/users.module'; // Müşteri yönetimi için eklendi
-import { PrismaModule } from './prisma/prisma.module'; // DB bağlantısı için eklendi
-import { CloudinaryModule } from './cloudinary/cloudinary.module'; // 👈 YENİ: Resim yükleme motoru eklendi
+import { UsersModule } from './users/users.module'; 
+import { PrismaModule } from './prisma/prisma.module'; 
+import { CloudinaryModule } from './cloudinary/cloudinary.module'; 
 
 @Module({
   imports: [
@@ -13,7 +15,9 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module'; // 👈 YENİ
     AuthModule,       // Giriş ve yetkilendirme
     OrdersModule,     // Sipariş işlemleri
     UsersModule,      // Müşteri kayıt ve sorgulama
-    CloudinaryModule, // 👈 YENİ: Resimlerin Cloudinary'e gitmesi için eklendi
+    CloudinaryModule, // Resimlerin Cloudinary'e gitmesi için eklendi
   ],
+  controllers: [AppController], // 👈 İŞTE 404 HATASINI ÇÖZEN SATIR! Kapıyı açtık.
+  providers: [AppService],      // 👈 İşçimizin görevlerini resmi olarak başlattık.
 })
 export class AppModule {}
